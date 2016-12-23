3de8a382d53c3655aaa1d0ba9211f554052b47ce
Gabriel Garavit <garavitgabriel@gmail.com>
2016-12-22 22:48:50 -0600

    Added ReactJS, compiled styles with Gulp and made the first layout for Admin

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -16,6 +16,8 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
+
+        return view('admin.users.index');
     }
 
     /**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,4 +20,13 @@ Route::auth();
 Route::get('/home', 'HomeController@index');
 
 
-Route::resource('admin/users', AdminUsersController );
+
+Route::get('/admin', function() {
+
+    return view('admin.index');
+});
+
+
+
+
+Route::resource('admin/users', 'AdminUsersController' );
